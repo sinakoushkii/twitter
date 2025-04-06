52f0c8d26b9ab0bb8976e94a7192e835aff8d7b0
sina koushki <sinaderik7@gmail.com>
2025-04-06 20:31:47 +0330

    add breakpoints to the containers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="flex justify-between">
-          <div>
+        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
+          <div className="h-screen px-2 xsm:px-4 xxl:px-8">
             <Leftbar />
           </div>
-          <div>{children}</div>
-          <div>
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] h-screen">{children}</div>
+          <div className="hidden lg:flex ml-4 md:ml-8 flex-1= h-screen">
             <Rightbar />
           </div>
         </div>

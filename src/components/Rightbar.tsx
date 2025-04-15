8b2b671b1deb886090d34a6dc61bfa90494fbb5a
sina koushki <sinaderik7@gmail.com>
2025-04-15 15:46:44 +0330

    add footer to the rightbar

--- src/components/Rightbar.tsx
+++ src/components/Rightbar.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Search from "./Search";
+import PopularTags from "./PopularTags";
+import Recommendations from "./Recommendations";
+import Link from "next/link";
 
 const Rightbar = () => {
   return (
-    <div>Rightbar</div>
-  )
-}
+    <div className="pt-4 flex flex-col gap-4 sticky top-0 h-max">
+      <Search />
+      <PopularTags />
+      <Recommendations />
 
-export default Rightbar
+      <div className="text-textGray text-sm flex gap-x-4 flex-wrap">
+        <Link href="/">Terms of Service</Link>
+        <Link href="/">Privacy Policy</Link>
+        <Link href="/">Cookie Policy</Link>
+        <Link href="/">Accessibility</Link>
+        <Link href="/">Ads Info</Link>
+        <span>© 2025 L Corp.</span>
+      </div>
+    </div>
+  );
+};
+
+export default Rightbar;

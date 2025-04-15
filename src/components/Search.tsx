09c2d3eaafeaa6a361019763ba4329f6d8675719
sina koushki <sinaderik7@gmail.com>
2025-04-15 15:54:39 +0330

    Search component initialized

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Image from "./Image";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="bg-inputGray py-2 px-4 flex items-center gap-4 rounded-full">
+      <Image path="icons/explore.svg" alt="search" w={16} h={16} tr={false} />
+      <input
+        type="text"
+        placeholder="Search"
+        className="bg-transparent border-none outline-none placeholder:text-textGray focus:outline-none focus:border-blue-500 w-full mt-2"
+      />
+    </div>
+  );
+};
 
-export default Search
+export default Search;
